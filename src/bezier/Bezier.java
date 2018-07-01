4d7ec869c9a21ac0058ca77fa70fb5d74ee52c23
Nozomu Hatta <nozomuhatta@gmail.com>
2018-07-01 12:25:51 +0900

    Add start button.

--- src/bezier/Bezier.java
+++ src/bezier/Bezier.java
@@ -4,10 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -31,12 +33,20 @@ public class Bezier extends JComponent{
 	}
 	
 	public Bezier() {
+		StartButton start_button = new StartButton();
+		start_button.setVisible(true);
+		JPanel button_panel = new JPanel();
+		button_panel.setVisible(true);
+		button_panel.setLayout(new GridLayout(1, 1));
+		button_panel.add(start_button);
+		
 		canvas_ = new BezierCanvas();
 		setLayout(new BorderLayout());
 		add(canvas_, BorderLayout.CENTER);
+		add(button_panel, BorderLayout.PAGE_END);
 		setVisible(true);
 	}
-	
+		
 	class BezierCanvas extends JPanel{
 
 		/**
@@ -59,5 +69,17 @@ public class Bezier extends JComponent{
 			g2.setColor(Color.BLACK);
 			g2.fillOval((int)p.getX(), (int)p.getY(), 5, 5);
 		}
+	}
+	
+	class StartButton extends JButton{		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public StartButton() {
+			super("Start");
+			setForeground(Color.GRAY);
+		}		
 	}
 }
