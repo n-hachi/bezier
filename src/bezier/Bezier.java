a1f1af5fc3c3eae2f5cf16f32877ea255ac0fc98
Nozomu Hatta <nozomuhatta@gmail.com>
2018-07-01 11:18:27 +0900

    Make it draws minimal window.

---
+++ src/bezier/Bezier.java
@@ -0,0 +1,11 @@
+package bezier;
+
+import javax.swing.JFrame;
+
+public class Bezier {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("bezier");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
