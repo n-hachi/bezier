bf6a0d5225d1a016daf688628d17d0e7954475af
Nozomu Hatta <nozomuhatta@gmail.com>
2018-07-03 00:07:57 +0900

    Add to draw line.

--- src/bezier/Bezier.java
+++ src/bezier/Bezier.java
@@ -7,9 +7,11 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.util.ListIterator;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -55,23 +57,29 @@ public class Bezier extends JComponent{
 	}
 	
 	void addBezierPoint(BezierPoint p) {
-		if(bezier_point_vec_.size() < BASE_DOTS_COUNT) {
-			bezier_point_vec_.add(p);
-			BufferedImage buf = new BufferedImage(400, 400, BufferedImage.TYPE_3BYTE_BGR);
-			Graphics2D buf_g = buf.createGraphics();
-			buf_g.setBackground(getBackground());
-			buf_g.clearRect(0, 0, 400, 400);
-
-			for(BezierPoint bp : bezier_point_vec_) {
-				bp.drawPoint(buf_g);
-			}
-			Graphics g = getGraphics();
-			g.drawImage(buf,  0,  0,  this);
-
+		if(bezier_point_vec_.size() > BASE_DOTS_COUNT) {
+			return;
 		}
+
+		bezier_point_vec_.add(p);
+		for(BezierPoint bp : bezier_point_vec_) {
+			canvas_.drawPoint(bp);
+		}
+		
+		if (bezier_point_vec_.size() >= 2) {
+			ListIterator<BezierPoint> iter1 = bezier_point_vec_.listIterator(0);
+			ListIterator<BezierPoint> iter2 = bezier_point_vec_.listIterator(1);
+			while(iter2.hasNext()) {
+				canvas_.drawLine(iter1.next(), iter2.next());
+			}
+			
+		}
+		canvas_.repaint();
 	}
 			
 	class BezierCanvas extends JPanel{
+		BufferedImage buf_;
+		Graphics2D buf_g_;
 
 		/**
 		 * 
@@ -86,10 +94,29 @@ public class Bezier extends JComponent{
 					Bezier.this.addBezierPoint(new BezierPoint(e.getPoint()));
 				}
 			});
+			setDoubleBuffered(true);
+			buf_ = new BufferedImage(400, 400, BufferedImage.TYPE_3BYTE_BGR);
+			buf_g_ = buf_.createGraphics();
+			buf_g_.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			buf_g_.setBackground(getBackground());
+			buf_g_.clearRect(0, 0, 400, 400);
+		}
+		
+		public void drawPoint(BezierPoint bp) {
+			buf_g_.setColor(Color.BLACK);
+			int diameter = bp.radius_ * 2;
+			buf_g_.fillOval(bp.x - bp.radius_, bp.y - bp.radius_, diameter, diameter);
+		}
+		
+		public void drawLine(BezierPoint bp1, BezierPoint bp2) {
+			buf_g_.setColor(Color.BLACK);
+			buf_g_.drawLine(bp1.x, bp1.y, bp2.x, bp2.y);
 		}
 		
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			g.drawImage(buf_, 0, 0, this);
+			buf_g_.clearRect(0, 0, 400, 400);
 		}
 	}
 	
